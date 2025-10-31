14e2136b52b2f343756c913736cc3e6eee916177
AndresH4 <andreshernandezparra04@gmail.com>
2025-10-31 07:36:15 -0500

    Subida de archivos
    Subo los 4 archivos de clase

---
+++ Ejercicios_1.py
@@ -0,0 +1,22 @@
+nombre="Andres"
+print("Hola Andres")
+print(nombre)
+numero1=45
+numero2=29
+suma=numero1+numero2
+print(suma)
+numero4=64
+numero5=38
+resta=numero4-numero5
+print(resta)
+numero6=7
+numero7=5
+multiplicacion=numero6*numero7
+print(multiplicacion)
+numero8=20
+numero9=4
+division=numero8/numero9
+print(division)
+entrada=input("escriba un numero")
+print(entrada)
+numero=int(entrada)
---
+++ Lista_1.py
@@ -0,0 +1,8 @@
+lista=[]
+#lista.insert(9)
+#lista.insert(5)
+#lista.insert(7)
+lista.append(10)
+lista.append(4)
+lista.append(6)
+print(lista)
